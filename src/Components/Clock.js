139cd2bf1198efa1557233864e3fa706e9e41f96
Esha Patel <Patellesha11@gmail.com>
2025-04-17 11:44:40 +0300

    added button reset

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -39,6 +39,7 @@ function Clock () {
             <div className="button">
                 <button  onClick={startTimer} className="start">START</button>
                 <button  onClick={stopTimer} className="stop">STOP</button>
+                <button  onClick={resetTimer} className="start">START</button>
             </div>
         </div>
     )
